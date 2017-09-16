018839f984fec111cb87f2359e9a11e3283183e9
Arslan Cheema <arslanch33ma@gmail.com>
2017-09-16 20:54:22 +0900

    Anonymous User Login

--- SocialAppStart/app/src/main/java/com/example/aarshad/socialappstart/Login.java
+++ SocialAppStart/app/src/main/java/com/example/aarshad/socialappstart/Login.java
@@ -1,5 +1,6 @@
 package com.example.aarshad.socialappstart;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
@@ -8,12 +9,21 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.VisibleForTesting;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
     EditText etName;
@@ -23,6 +33,12 @@ public class Login extends AppCompatActivity {
 
     final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
     int RESULT_LOAD_IMAGE = 346;
+
+    public static final String TAG = "AnonymousTag";
+    FirebaseAuth mAuth ;
+
+    FirebaseAuth.AuthStateListener mAuthStateListener ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,24 +55,82 @@ public class Login extends AppCompatActivity {
             }
         });
 
+        mAuth  = FirebaseAuth.getInstance();
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user!=null){
+                    Log.d(TAG,"onAuthStateChanged: SignedIn: " + user.getUid());
+                } else{
+
+                    Log.d(TAG,"onAuthStateChanged: SignedOut: ");
+                }
+            }
+        };
+
     }
 
     public void buLogin(View view) {
 
     }
 
-
     @Override
     public void onStart() {
         super.onStart();
-
+        mAuth.addAuthStateListener(mAuthStateListener);
+        signInAnonymously();
     }
 
     @Override
     public void onStop() {
         super.onStop();
-
+        if (mAuthStateListener != null) {
+            mAuth.removeAuthStateListener(mAuthStateListener);
+        }
+        hideProgressDialog();
     }
+    private void signInAnonymously() {
+
+        mAuth.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInAnonymously:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInAnonymously", task.getException());
+
+                        }
+
+                    }
+                });
+    }
+
+    @VisibleForTesting
+    public ProgressDialog mProgressDialog;
+
+    public void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage("loading");
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.show();
+    }
+
+    public void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
 
     void checkUserPermission(){
         if ( Build.VERSION.SDK_INT >= 23){
